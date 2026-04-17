8ebdb818d7a2ef3d396ee37546c021ba71313fea
GiuliaTrz <148765503+GiuliaTrz@users.noreply.github.com>
2026-04-17 14:23:02 +0200

    Fixed models

--- app/src/main/java/it/unicam/cs/mpgc/rpg126763/models/NPC.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126763/models/NPC.java
@@ -3,19 +3,29 @@ package it.unicam.cs.mpgc.rpg126763.models;
 public class NPC {
 
     private String name;
+    private String dialogueHint; // piccola estensione utile
 
-    @Override
-    public String toString() {
-        return "NPC{" +
-                "name='" + name + '\'' +
-                '}';
+    public NPC() {}
+
+    public NPC(String name) {
+        this.name = name;
+    }
+
+    public NPC(String name, String dialogueHint) {
+        this.name = name;
+        this.dialogueHint = dialogueHint;
     }
 
     public String getName() {
         return name;
     }
 
-    public NPC (String name){
-        this.name = name;
+    public String getDialogueHint() {
+        return dialogueHint;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
