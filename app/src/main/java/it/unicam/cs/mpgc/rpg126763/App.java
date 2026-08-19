017c4c23fa752a6ac59a4e82c31f925ce45cceeb
GiuliaTrz <148765503+GiuliaTrz@users.noreply.github.com>
2026-08-19 22:43:24 +0200

    demo ui

--- app/src/main/java/it/unicam/cs/mpgc/rpg126763/App.java
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126763/App.java
@@ -3,6 +3,11 @@ package it.unicam.cs.mpgc.rpg126763;
 import it.unicam.cs.mpgc.rpg126763.character.Nemico;
 import it.unicam.cs.mpgc.rpg126763.engine.GameEngine;
 import it.unicam.cs.mpgc.rpg126763.ui.ConsoleUI;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -13,8 +18,22 @@ import java.util.Scanner;
  * Mostra un menu per la scelta tra nuova partita e caricamento,
  * quindi delega l'esecuzione al {@link GameEngine} tramite la UI console.
  */
-public class App {
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
+
+        launch();
+
+       /*
         ConsoleUI ui = new ConsoleUI();
         Scanner scanner = new Scanner(System.in);
 
@@ -50,5 +69,9 @@ public class App {
             String name = scanner.nextLine();
             engine.startNewGame(name.isBlank() ? "Linden" : name);
         }
+         */
+
+
     }
+
 }
